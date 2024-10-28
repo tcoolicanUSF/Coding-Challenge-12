4b6d54d0fc86ee7705140c515cdad133d3ba6699
tcoolican <tcoolican@usf.edu>
2024-10-27 20:32:05 -0400

    Implement color selection and canvas clearing functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const canvas = document.getElementById('canvas-element')
+const ctx = canvas.getContext('2d');
+const clearCanvasButton = document.getElementById('clear-canvas')
+const colorInput = document.getElementById('choose-color')  
 
+//Task 4: Add color selection and Canvas Clearing
+colorInput.addEventListener('input', (event) =>{
+    color = event.target.value
+} )
+clearCanvasButton.addEventListener('click', () => {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+})
